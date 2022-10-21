c43d8e96b8cc9cf72db917e34226e54453e1512e
Siddharth Suresh <siddh.suresh@gmail.com>
2022-10-21 11:27:55 +0530

    fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://siddharthsuresh.vercel.app',
   integrations: [mdx(), sitemap(), tailwind(), compress()]
 });
